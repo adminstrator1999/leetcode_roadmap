b7465c70d0720fbec5dccdb8db2792e6776db077
Bahromjon <hopebegmuhammadov@gmail.com>
2023-08-16 20:53:38 +0500

    solved completely

--- problems/palindrome_number.py
+++ problems/palindrome_number.py
@@ -3,17 +3,52 @@ Given an integer x, return true if x is a palindrome, and false otherwise.
 https://leetcode.com/problems/palindrome-number/
 """
 
+
 class Solution:
+
     def is_palindrome(self, x: int) -> bool:
+        # solving this without changing this into string
+        if x < 0:
+            return False
+        reversed_x = 0
+        temp_x = x
+        while temp_x:
+            reversed_x *= 10
+            reversed_x += temp_x % 10
+            temp_x //= 10
+        return reversed_x
+
+    def is_palindrome_bad(self, x: int) -> bool:
+        # solving this without changing this into string
+        if x < 0:
+            return False
+        numbers = []
+        while x:
+            numbers.append(x % 10)
+            x //= 10
+        end = len(numbers) - 1
+        begin = 0
+        while begin < end:
+            if numbers[begin] != numbers[end]:
+                return False
+            end -= 1
+            begin += 1
+        return True
+
+    def is_palindrome_brute_force(self, x: int) -> bool:
         # brute force approach
         # convert into string and use two pointers
         string_x = str(x)
         length = len(string_x)
-        end = length - 1 
-        begin = 0 
+        end = length - 1
+        begin = 0
         while begin < end:
             if string_x[begin] != string_x[end]:
                 return False
             end -= 1
             begin += 1
         return True
+
+
+solution = Solution()
+print(solution.is_palindrome(100))
