a7a8bb7ccc971c947c03f317ab1d7f481261ab51
Bahromjon <hopebegmuhammadov@gmail.com>
2023-08-18 15:35:31 +0500

    generate parentheses is solved

--- stack/evaluate_reverse_polish_notation.py
+++ stack/evaluate_reverse_polish_notation.py
@@ -2,7 +2,7 @@
 You are given an array of strings tokens that represents an arithmetic
  expression in a Reverse Polish Notation.
 
-Evaluate the expression. 
+Evaluate the expression.
 Return an integer that represents the value of the expression.
 
 Note that:
